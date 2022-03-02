17408b32d239abd407f6ae49f8e74b9051ec1b70
milos <milos_divljak@live.com>
2022-03-02 13:14:16 +0100

    added FlightsResultsPage, modified BookingFlightTest, gitignore and Runner.xml

--- src/test/java/tests/BookingFlightTest.java
+++ src/test/java/tests/BookingFlightTest.java
@@ -1,12 +1,17 @@
 package tests;
 
 import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import pages.BasePage;
 import pages.FlightsPage;
+import pages.FlightsResultsPage;
+
+import java.io.IOException;
 
 
 public class BookingFlightTest extends BaseTest {
@@ -28,16 +33,31 @@ public class BookingFlightTest extends BaseTest {
     @Test(description = "Booking a flight")
     @Description ("Simple end to end test")
     @Parameters
-            ({"whereFromLocation", "whereToLocation", "departDate", "returnDate"})
-    public void bookingFlightTest(String whereFromLocation, String whereToLocation, String departDate, String returnDate) throws InterruptedException {
+            ({"whereFromLocation", "whereToLocation", "departDate", "returnDate", "indexOfCheckBox", "filters", "description"})
+    public void bookingFlightTest(String whereFromLocation,
+                                  String whereToLocation,
+                                  String departDate,
+                                  String returnDate,
+                                  int indexOfCheckBox,
+                                  String filters,
+                                  String description)
+            throws InterruptedException, IOException {
 
         BasePage basePage = new BasePage(driver);
         FlightsPage flightsPage = new FlightsPage(driver);
+        FlightsResultsPage flightsResultsPage = new FlightsResultsPage(driver);
 
         flightsPage.addWhereFromLocation(whereFromLocation);
         flightsPage.addWhereToLocation(whereToLocation);
         flightsPage.setDates(departDate, returnDate);
         flightsPage.clickSearch();
+        flightsResultsPage.dragSliderFilter();
+        flightsResultsPage.clickFlightTimesCheckBox(indexOfCheckBox);
+        basePage.scrollByPixels("400");
+        reportScreenshot(filters,description);
+        Assert.assertEquals("true", driver.findElements(By.xpath("//div[@class='css-icaorl']//input[@type='checkbox']")).get(2).getAttribute("checked"));
+        flightsResultsPage.clickFlightButton();
+        flightsResultsPage.clickSelectButton();
 
     }
 
